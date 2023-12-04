2e604b745a3f87ece544e9f90bdbe31f0b5c3715
Вадим <va.arutyunov@gmail.com>
2023-12-04 13:48:32 +0300

    done homework

---
+++ src/Books.java
@@ -0,0 +1,29 @@
+public class Books {
+    private String title;
+    private int year;
+    private Author author;
+
+    public Books (String title, int year, Author author) {
+        this.title = title;
+        this.year = year;
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+    public int getYear() {
+        return year;
+    }
+    public Author getAuthor() {
+        return author;
+    }
+
+
+
+
+}
+
+
+
+
